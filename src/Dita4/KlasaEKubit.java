493a3879b6ce7a242fe140b9c8e4f5a447423514
eraldmullaraj <erald.mularaj@gmail.com>
2017-06-11 12:21:28 +0200

    ndryshim te bera diten e djele

--- src/Dita4/KlasaEKubit.java
+++ src/Dita4/KlasaEKubit.java
@@ -6,7 +6,7 @@ package Dita4;
 public class KlasaEKubit {
     public static void main (String[] args){
         Kubi kubi1 = new Kubi(20, 30,40);
-        Kubi kubi2 = new Kubi();
+        Kubi kubi2 = new Kubi(); // Kubi default
 
         System.out.println(kubi1.getCubeVolume());
         System.out.print(kubi2.getCubeVolume());
--- src/Dita4/KlasaQershor.java
+++ src/Dita4/KlasaQershor.java
@@ -9,10 +9,16 @@ public class KlasaQershor {
 
         int mbledhja = Mblidh(200, 45, 222);
         int rroga = mbledhja * 20000;
-        System.out.print(rroga);
-    }
+        double RrMesatare = Mesatarja(205, 30, 40);
+        System.out.println(rroga);
+        System.out.println(RrMesatare);
 
+    }
+    // Krijohen metodat dhe perdoren me larte
     public static int Mblidh(int a, int b, int c){
         return(a+b+c);
+    }
+    public static double Mesatarja(int a, int b, int c){
+        return(a+b+c)/3;
     }
 }
